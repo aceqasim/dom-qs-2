929067d22047163817e83db23344c52801613887
aceqasim <m.kaxim21@gmail.com>
2025-07-15 21:04:59 +0500

    dom part 1 practice

---
+++ script.js
@@ -0,0 +1,8 @@
+let heading = document.getElementById('heading') 
+console.dir(heading)
+
+let allHeadings = document.getElementsByClassName('heading')
+console.dir(allHeadings)
+
+let div = document.querySelector("div").children
+console.log(div)
